f4a18dcdb5e4a0ac3dc6b7ac325906df13e73aa4
saad <167574691+saad-rao@users.noreply.github.com>
2024-08-16 20:24:30 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+
+                    //   ALERT
+
+
+// var name1=('my name is saad naseem')
+// alert(name1)
+
+var email = "saadnaseem123@gmail.com"
+alert("My email address is" + ' '+ email)
+
+
+var book = "A smarter way to learn javascriipt"
+alert("I am trying to learn from the book"+' '+book)
+
+
+                      //    VARIABLE FOR STRING
+
+// var myName = ('My name is Saad naseem')
+// document.write(myName);
+
+
+
+                       // variable for number
+
+
+//  var num1=10
+//  var num2=10
+//  document.write(num1+num2)                       
